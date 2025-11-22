6ed16a0a87111afc3fad46ae9c9cd06f9280aeda
Luis Valdo <luisvso.dev@gmail.com>
2025-11-22 17:54:01 -0300

    Adicionado microserviceapplication

--- src/main/java/com/example/microservicoDeProfessor/MicroservicoDeProfessorApplication.java
+++ src/main/java/com/example/microservicoDeProfessor/MicroservicoDeProfessorApplication.java
@@ -1,22 +1,15 @@
 package com.example.microservicoDeProfessor;
 
-import com.netflix.discovery.shared.transport.jersey3.Jersey3TransportClientFactories;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class MicroservicoDeProfessorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MicroservicoDeProfessorApplication.class, args);
-	}
-
-    @Bean
-    public Jersey3TransportClientFactories jersey3TransportClientFactories() {
-        return new Jersey3TransportClientFactories();
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicoDeProfessorApplication.class, args);
     }
 
 }
